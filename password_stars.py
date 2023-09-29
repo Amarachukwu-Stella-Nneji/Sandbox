a7142714d525880f3346d4007213e4e8170d273e
Stella <amarachukwustella.nneji@my.jcu.edu.au>
2023-09-29 11:32:21 +1000

    List of files

--- password_stars.py
+++ password_stars.py
@@ -3,4 +3,4 @@ password = input("Enter a password: ")
 while len(password) < MINIMUM_LENGTH:
     password = input("Enter a password: ")
 asterisks = '*' * len(password)
-print(f"Password: {asterisks}.")
+print(f"Password: {asterisks}")
