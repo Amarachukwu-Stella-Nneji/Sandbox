2f152000275704b60b00f37231ff2f1d7299078a
Stella <amarachukwustella.nneji@my.jcu.edu.au>
2023-09-29 11:33:36 +1000

    List of files

--- list_files.py
+++ list_files.py
@@ -4,4 +4,4 @@ print(f"The files and folders in {os.getcwd()} are:")
 items = os.listdir('.')
 for item in items:
     prefix = "(d) " if os.path.isdir(item) else "(f) "
-    print(f"{prefix}\t{item}.")
+    print(f"{prefix}\t{item}")
